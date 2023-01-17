7bed1baf074491f7b49cd0a9c8aeb052c976988d
Timothy Jordan <121692742+tljorda2@users.noreply.github.com>
2023-01-17 12:03:30 -0700

    Add files via upload

---
+++ CBB Prediction/CBB_CrossFold.py
@@ -0,0 +1,121 @@
+#%%
+from CBB_DataPrep import *
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#%%
+# Splitting the data into X and y. Dropped any of the features we are not interested in.
+X = Model_DataFrame.drop(columns=['Schl', 'Opp', 'School_x', 'Home_Win', 'School_y'],axis=1)
+y = Model_DataFrame['Home_Win']
+
+#%%
+#Importing the metrics we are going to use
+from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score, f1_score
+# Defining a dictionary of metrics
+score_metrics = {'accuracy':make_scorer(accuracy_score),
+                 'precision': make_scorer(precision_score),
+                 'recall': make_scorer(recall_score),
+                 'f1_score': make_scorer(f1_score)}
+
+# Since I will be constantly repeating these lines of codes with few changes, I decided to make a function
+# With the inputs being what is mostly going to be changed throughout the file.
+def RunModel(model, folds=10):
+    if type(folds) == int:
+        pass
+    else:
+        raise TypeError('The number of folds has to be an integer')
+    scores = cross_validate(model, X, y, cv=folds, scoring=score_metrics)
+    print('Avg Accuracy: '+str(np.mean(scores['test_accuracy'])))
+    print('Avg Precision: '+str(np.mean(scores['test_precision'])))
+    print('Avg Recall: '+str(np.mean(scores['test_recall'])))
+    print('Avg F1-Score: '+str(np.mean(scores['test_f1_score'])))
+    return np.mean(scores['test_accuracy'])
+#%%
+'''K-Nearest Neighbors'''
+from sklearn.model_selection import cross_validate
+# Importing the KNN Module
+from sklearn.neighbors import KNeighborsClassifier
+
+#Create an instance of the model
+knn = KNeighborsClassifier(n_neighbors=5)
+
+#Evaluating the model via 10 fold cross validation
+KNN_CF_Accuracy = RunModel(knn, 10)
+
+# %%
+'''Neural Network'''
+# Importing the nn Module
+from sklearn.neural_network import MLPClassifier
+
+#Create an instance of the model
+# Adjusted the max iterations since there was a convergence warning at 200 iterations
+nn = MLPClassifier(hidden_layer_sizes=(5,3),random_state=1, max_iter=500)
+
+#Evaluating the model via 10 fold cross validation
+NeuralNet_CF_Accuracy = RunModel(nn, 10)
+# %%
+'''Decision Tree'''
+# Importing the dt Module
+from sklearn import tree
+
+#Create an instance of the model
+dt = tree.DecisionTreeClassifier()
+
+#Evaluating the model via 10 fold cross validation
+DecisionTree_CF_Accuracy = RunModel(dt, 10)
+#%%
+'''Logistic Regression'''
+from sklearn.linear_model import LogisticRegression
+
+log = LogisticRegression(max_iter=3000)
+LogRegression_CF_Accuracy = RunModel(log,10)
+
+#%%
+'''Random Forest'''
+from sklearn.ensemble import RandomForestClassifier
+ran_forest = RandomForestClassifier(n_estimators=100, random_state=100)
+RandomForest_CF_Accuracy = RunModel(ran_forest,10)
+
+#%%
+'''Naive Bayes'''
+from sklearn.naive_bayes import GaussianNB
+naive_bayes = GaussianNB()
+NaiveBayes_CF_Accuracy = RunModel(naive_bayes, 10)
+# %%
+# Importing the voting classifier
+from sklearn.ensemble import VotingClassifier
+
+#Creating instances of different classifiers to be aggregated
+clf1 = LogisticRegression(random_state=1)
+clf2 = RandomForestClassifier(n_estimators=50, random_state=1)
+clf3 = GaussianNB()
+
+#Applying the voting ensemble
+eclf = VotingClassifier(
+    estimators=[('lr', clf1), ('rf', clf2), ('gnb', clf3)],
+    voting='hard'
+)
+
+#Evaluate all Classifiers
+for clf, label in zip([clf1,clf2,clf3,eclf], 
+                      ['Logisitic Regression', 'Random Forest', 'Naive Bayes', 'Ensemble']
+                      ):
+  scores = cross_validate(clf, X, y, scoring=score_metrics, cv=10)
+  print('Accuracy: %0.2f [%s]' % (np.mean(scores['test_accuracy']), label))
+
+#%%
+from statistics import fmean
+Names = ['Classification Tree', 'Neural Network', 'K-Nearest Neighbors', 'Logistic Regression',
+         'Random Forest', 'Naive Bayes']
+Original_Accuracy = [DecisionTree_CF_Accuracy, NeuralNet_CF_Accuracy, KNN_CF_Accuracy,
+                     LogRegression_CF_Accuracy, RandomForest_CF_Accuracy, NaiveBayes_CF_Accuracy]
+# This compares the accuracy scores for all the models ran between using SMOTE and using the original dataset
+ax = sns.barplot(x=Original_Accuracy, y=Names)
+plt.xlabel(f'Accuracy Score')
+plt.xlim(0.70,0.82)
+plt.ylabel('Models')
+plt.title(f'Original Dataset Accruacy Scores\nAverage: {round(fmean(Original_Accuracy), 4)}\nAxis Condensed')
+plt.show()
+
+# %%
--- CBB Prediction/CBB_Prediction.py
+++ CBB Prediction/CBB_Prediction.py
@@ -5,109 +5,7 @@ import numpy as np
 import re
 import matplotlib.pyplot as plt
 import seaborn as sns
-#%%
-# Reading all of the data and converting them into dataframes
-team_stats = pd.read_html('https://www.sports-reference.com/cbb/seasons/2023-school-stats.html', header=1)
-oppenent_stats = pd.read_html('https://www.sports-reference.com/cbb/seasons/2023-opponent-stats.html', header=1)
-advanced_stats = pd.read_html('https://www.sports-reference.com/cbb/seasons/2023-advanced-school-stats.html', header=1)
-opp_advanced_stats = pd.read_html('https://www.sports-reference.com/cbb/seasons/2023-advanced-opponent-stats.html', header=1)
-# read_html turned it into a list that contains the dataframe
-# By indexing the 0th position of the list, it gives us the dataframe outside of the lsit
-team_stats = team_stats[0]
-oppenent_stats = oppenent_stats[0]
-advanced_stats = advanced_stats[0]
-opp_advanced_stats = opp_advanced_stats[0]
-print(oppenent_stats.info())
-# %%
-# Columns to be dropped
-# cols represents the columns dropped for the team stats dataframe. Some of the first few columns are repeated
-# in the other dataframes, so we can keep them in the first one, and drop them from the rest
-cols = [0,2,3,4,8,11,12,13,14,17,20]
-cols2 = [0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-
-team_stats.drop(columns=team_stats.columns[cols], axis=1,inplace=True)
-oppenent_stats.drop(columns=oppenent_stats.columns[cols2], axis=1,inplace=True)
-advanced_stats.drop(columns=advanced_stats.columns[cols2], axis=1,inplace=True)
-opp_advanced_stats.drop(columns=opp_advanced_stats.columns[cols2], axis=1,inplace=True)
-
-# Dropping all null values based on the rows
-team_stats.dropna(axis=0, inplace=True)
-opp_advanced_stats.dropna(axis=0, inplace=True)
-advanced_stats.dropna(axis=0, inplace=True)
-oppenent_stats.dropna(axis=0, inplace=True)
-
-#Dropping any duplicate values from the tables. I believe this dropped around 10 rows
-team_stats.drop_duplicates('School',inplace=True)
-opp_advanced_stats.drop_duplicates('School',inplace=True)
-advanced_stats.drop_duplicates('School',inplace=True)
-oppenent_stats.drop_duplicates('School',inplace=True)
-
-# %%
-# Comparing the information for all of the dataframes and making sure the value counts line up and are all not null
-print(team_stats.info())
-print(oppenent_stats.info())
-print(advanced_stats.info())
-print(opp_advanced_stats.info())
-
-#%%
-# Merging all of the stats dataframes into one.
-# Merged all of them left inter the team_statsdf on school name
-statsdf = pd.merge(left=team_stats,right=oppenent_stats,how='inner', on='School',)
-statsdf = pd.merge(left=statsdf, right=advanced_stats, on='School', how='inner')
-statsdf = pd.merge(left=statsdf, right=opp_advanced_stats,how='inner', on='School')
-print(statsdf)
-#%%
-# Oncew again, dropping any null values that were created with the merge
-statsdf.dropna(axis=0, inplace=True)
-statsdf.info()
-
-#%%
-# Since most of the data types listed above are "object", we need to convert those into a numeric form
-# we use label encoding to do this
-
-from sklearn.preprocessing import LabelEncoder
-#Initialize
-le = LabelEncoder()
-
-# Used a for loop since everything needed to be transformed into a numerical type aside from the first column, school
-columns = statsdf.keys()
-for i in columns:
-    if i == 'School':
-        pass
-    else:
-        statsdf[i] = le.fit_transform(statsdf[i])
-
-statsdf.info()
-
-# %%
-gamesdf = pd.read_csv('C:\\Users\\Timothy Jordan\\Desktop\\Pandas Practice\\CBB Prediction\\college_games.csv')
-gamesdf.info()
-# %%
-# Defining a list of columns to be dropped from the gamesdf. These are columns we are not interested in
-drop_col = ['Year', 'Date', 'Unnamed: 2', 'Rk.', 'Unnamed: 5', 'Rk..1'
-            , 'Unnamed: 9', 'PTS', 'OPP', 'MOV', 'Unnamed: 13', 'OT']
-gamesdf.drop(columns=drop_col, axis=1, inplace=True)
-gamesdf.dropna(axis=0,inplace=True)
-gamesdf.info()
-# %%
-# Renaming the unnamed column to Home_Win since this is going to be our target variable
-gamesdf.rename(columns={'Unnamed: 8': 'Home_Win'}, inplace=True)
-# Felt better by transforming it into a binary outcome of 1 and 0 with 1 representing a Home Team Win
-gamesdf['Home_Win'] = le.fit_transform(gamesdf['Home_Win'])
-gamesdf.head(10)
-# %%
-# Merging the individual games with the stats for the teams that played in those games
-# First merging the home team stats and then the away teasm stats
-# Used a left join and joined on the school names
-Model_DataFrame = pd.merge(left=gamesdf, right=statsdf, how='left', left_on='Schl', right_on='School')
-Model_DataFrame = pd.merge(left=Model_DataFrame, right=statsdf, how='left', left_on='Opp', right_on='School')
-print(Model_DataFrame.info())
-Model_DataFrame.head(5)
-# %%
-# Dropping null values once again from the final dataframe and checking to see if everything looks right
-Model_DataFrame.dropna(axis=0,inplace=True)
-# Had to use verbose and show_counts since it did not default to it for whatever reason
-Model_DataFrame.info(verbose=True, show_counts=True)
+from CBB_DataPrep import *
 # %%
 # Splitting the data into X and y. Dropped any of the features we are not interested in.
 X = Model_DataFrame.drop(columns=['Schl', 'Opp', 'School_x', 'Home_Win', 'School_y'],axis=1)
@@ -275,6 +173,7 @@ knn.fit(X_train, y_train)
 # Predicting the outcome
 y_pred = knn.predict(X_test)
 # Printing the classification report
+print('K-Nearest Neightbors')
 print('ORIGINAL DATA\n-------------')
 print(classification_report(y_test, y_pred, target_names=target))
 KNN_Accuracy = metrics.accuracy_score(y_test, y_pred)
@@ -514,7 +413,7 @@ def CBB_Prediction(home_team, away_team, model_type='log'):
         print(f'Estimated Probability of a win: {prob}%')
 
 #%%
-CBB_Prediction('Arizona', 'Southern California')
+CBB_Prediction('Missouri', 'Arkansas')
 # %%
 team_stats['School']
 # %%
